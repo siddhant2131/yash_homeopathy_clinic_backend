b99f85c478cd5e27b17ae061aff1b5e6e37dd633
Siddhant Jadhav <sidhujadhavkop@gmail.com>
2025-10-05 11:02:45 +0530

    server connection and DB Connection successfuly

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
